d7f6f1d77f2227e6b9b93d90a134a939c59bb1ac
kmoniker <caseywchristensen@gmail.com>
2017-11-25 14:29:58 -0600

    First version of application moved into github

---
+++ locallibrary/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+# Use include() to add URLS from the catalog application
+from django.conf.urls import include
+
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
